3ffb94b9a8f4f003a7029d525081db16237a6a8e
Mohamad Aboda <41721894+Mohamad-Aboda@users.noreply.github.com>
2021-01-13 19:18:27 +0200

    Update fishmodel.py

--- model/fishmodel.py
+++ model/fishmodel.py
@@ -1,28 +1,44 @@
-import pandas as pd 
-import matplotlib.pyplot as plt 
-import numpy as np 
-import seaborn as sns 
+
+
+
+import pandas as pd # reading and writing CSV etc 
+import numpy as np # handling mathematical functions
 import joblib
 
-df = pd.read_csv('Fish.csv')
-df =pd.get_dummies(df)
-X=df[['Length1', 'Length2', 'Length3', 'Height', 'Width',
-       'Species_Bream', 'Species_Parkki', 'Species_Perch', 'Species_Pike',
-       'Species_Roach', 'Species_Smelt', 'Species_Whitefish']]
+#Data Viz libraries
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
-
-y = df['Weight']
+#Scientific Learning libraries
+from scipy import stats
 from sklearn.model_selection import train_test_split
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
 from sklearn.linear_model import LinearRegression
-LR = LinearRegression()
-LR.fit(X_train,y_train)
-y_pred = LR.predict(X_test)
-print(f'the score is {LR.score(X_train, y_train)}')
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
+from sklearn.model_selection import cross_val_score
 
-from sklearn.metrics import mean_squared_error
-rms = np.sqrt(mean_squared_error(y_test,y_pred))
-print('rms Error :',rms)
 
-filename = 'finalmodel.sav'
+
+df=pd.read_csv('Fish.csv')
+
+
+y=df['Species']
+X=df.drop(['Species'],axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+LR=LogisticRegression()
+
+LR.fit(X_train,y_train)  #now will fit and predict
+
+y_pred=LR.predict(X_test)
+
+LR.score(X_test,y_test)
+
+data={'Actual':y_test.array,'Predicted':y_pred}
+res=pd.DataFrame(data)
+
+filename = 'finalFishModel.sav'
 joblib.dump(LR, filename)
